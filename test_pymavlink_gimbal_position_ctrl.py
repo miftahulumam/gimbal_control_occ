a758133014928873c4254e9b60eb43b99b58c614
miftahulumam <miftahul.umam14@gmail.com>
2025-04-05 12:20:03 +0900

    first commit

---
+++ test_pymavlink_gimbal_position_ctrl.py
@@ -0,0 +1,75 @@
+import time
+from pymavlink import mavutil
+
+# degree command
+deg = {'yaw': 90, 'pitch': 0, 'roll': 0}
+
+# start connection
+conn = mavutil.mavlink_connection("/dev/ttyUSB0")
+
+# wait for first heartbeat
+print("Waiting for Heartbeat...")
+conn.wait_heartbeat()
+print(f"Heartbeat from system (system {conn.target_system} component {conn.target_component})")
+
+# gimbal control 
+print(f"Controlling gimbal: \nyaw = {deg['yaw']} \npitch = {deg['pitch']} \nroll = {deg['roll']}")
+conn.mav.command_long_send(conn.target_system, 
+                           conn.target_component,
+                           mavutil.mavlink.MAV_CMD_DO_MOUNT_CONTROL,
+                           0,
+                           # the parameters
+                deg['pitch'],   # parameter 1: pitch
+                 deg['roll'],   # parameter 2: roll
+                  deg['yaw'],   # parameter 3: yaw
+                           0,   # parameter 4
+                           0,   # parameter 5
+                           0,   # parameter 6: input mode (position or speed)
+                           2    # parameter 7
+                           )
+
+# msg = conn.recv_match(type='COMMAND_ACK', blocking=True)
+# print(msg)
+print(f"Maneuver done!\n")
+
+time.sleep(2)
+
+print(f"Controlling gimbal: \nyaw = {-deg['yaw']} \npitch = {-deg['pitch']} \nroll = {-deg['roll']}")
+conn.mav.command_long_send(conn.target_system, 
+                           conn.target_component,
+                           mavutil.mavlink.MAV_CMD_DO_MOUNT_CONTROL,
+                           0,
+                    ###    the parameters
+               -deg['pitch'],   # parameter 1: pitch
+                -deg['roll'],   # parameter 2: roll
+                 -deg['yaw'],   # parameter 3: yaw
+                           0,   # parameter 4
+                           0,   # parameter 5
+                           0,   # parameter 6: input mode (position or speed)
+                           2    # parameter 7
+                           )
+
+# msg = conn.recv_match(type='COMMAND_ACK', blocking=True)
+# print(msg)
+print(f"Maneuver done!\n")
+
+time.sleep(2)
+
+print(f"Controlling gimbal: \nyaw = 0 \npitch = 0\nroll = 0")
+conn.mav.command_long_send(conn.target_system, 
+                           conn.target_component,
+                           mavutil.mavlink.MAV_CMD_DO_MOUNT_CONTROL,
+                           0,
+                    ###    the parameters
+                           0,   # parameter 1: pitch
+                           0,   # parameter 2: roll
+                           0,   # parameter 3: yaw
+                           0,   # parameter 4
+                           0,   # parameter 5
+                           0,   # parameter 6: input mode (position or speed)
+                           2    # parameter 7
+                           )
+
+# msg = conn.recv_match(type='COMMAND_ACK', blocking=True)
+# print(msg)
+print(f"Maneuver done!\n")
